edd97d267651d2facbca0d3649477efba4f7f3de
Alex Bourt <alex@bourt.com>
2020-11-12 12:46:22 +0100

    first upload

---
+++ Build.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace autojoin
+{
+    class Build
+    {
+        public string       InputFile;
+        public string       InputDir;
+
+        public List<string> InputFiles;
+
+        public string       OutputFile;
+                            
+
+        string              m_lastChangedFile;
+                              
+        public Timer        m_dupTimer;
+        FileSystemWatcher   m_watcher;
+
+        public Build(string inFile, string outFile)
+        {
+            InputFile = Path.GetFullPath(inFile);
+
+            if (!File.Exists(InputFile))
+            {
+                Console.Write("\nInvalid input list file\n");
+                return;
+            }
+
+
+            InputDir = Environment.CurrentDirectory;
+            UpdateInputFiles();
+
+            OutputFile = Path.GetFullPath(Path.Combine(InputDir, outFile));
+            JoinFiles(false);
+        }
+
+        public void UpdateInputFiles()
+        {
+            InputFiles = new List<string>();
+            ReadFile(InputFile, ref InputFiles);
+        }
+
+
+        void ReadFile(string inputFile, ref List<string> inputFiles)
+        {
+            var sr = new StreamReader(new FileStream(
+                inputFile,
+                FileMode  .Open,
+                FileAccess.Read,
+                FileShare .ReadWrite));
+
+            while (!sr.EndOfStream)
+            {
+                var file = sr.ReadLine().Trim();
+
+                if (file == "")
+                    continue;
+
+                if (   file.Length >= 2
+                    && file.Substring(0, 2) == "//")
+                    continue;
+
+
+                if (   file[0            ] == '['
+                    && file[file.Length-1] == ']')
+                {
+                    file = file.Substring(1, Math.Max(0, file.Length-2));
+
+                    ReadFile(
+                        Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, file)), 
+                        ref inputFiles);
+                }
+                
+                else
+                    inputFiles.Add(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, file)));
+            }
+        }
+
+
+        public void JoinFiles(bool same)
+        {
+            using (var output = new StreamWriter(OutputFile, false))
+            { 
+                var hasErrors = false;
+    
+                for (int i = 0; i < InputFiles.Count; i++)
+                {
+                    var file = InputFiles[i];
+
+                    if (File.Exists(file))
+                    {
+                        var sr = new StreamReader(new FileStream(
+                            file,
+                            FileMode.Open,
+                            FileAccess.Read,
+                            FileShare.ReadWrite));
+
+                        output.Write(sr.ReadToEnd());
+
+                        if (i < InputFiles.Count - 1)
+                            output.Write("\n\n\n");
+                    }
+                    else if (!same)
+                    {
+                        if (!hasErrors) 
+                        {
+                            Console.Write("\n");
+                            hasErrors = true;
+                        }
+
+                        Console.Write("  Missing file [" + file + "]\n");
+                    }
+                }
+
+                if (hasErrors) 
+                    Console.Write("\n");
+            }
+        }
+
+        void OnFileChanged(object src, FileSystemEventArgs e)
+        {
+            if (e.FullPath == InputFile)
+                UpdateInputFiles();
+
+            else if (!InputFiles.Contains(e.FullPath))
+                return;
+
+            var same = e.Name == m_lastChangedFile;
+
+            if (!same)
+            {
+                Console.Write("* " + e.Name + " -> " + Path.GetFileName(OutputFile) + "\n");
+                StartLastFileTimer();
+            }
+
+            m_lastChangedFile = e.Name;
+
+            JoinFiles(!same);
+        }
+
+
+        public void Watch()
+        {
+            m_watcher = new FileSystemWatcher();
+
+            m_watcher.Path                  = InputDir;
+            m_watcher.Filter                = "*.*";
+            m_watcher.NotifyFilter          = NotifyFilters.LastWrite;
+            m_watcher.EnableRaisingEvents   = true;
+            m_watcher.IncludeSubdirectories = true;
+
+            m_watcher.Changed += OnFileChanged;
+
+            Console.Write("Watching [" + Path.GetFileName(InputFile) + "] for file changes...\n");
+        }
+
+
+        void StartLastFileTimer()
+        {
+            m_dupTimer = new Timer(500);
+
+            m_dupTimer.AutoReset = false;
+            m_dupTimer.Enabled   = true;
+
+            m_dupTimer.Elapsed += OnTimer;
+        }
+
+        void OnTimer(Object src, ElapsedEventArgs e)
+        {
+            m_lastChangedFile = "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Timers;
+//using System.Threading;
+
+namespace autojoin
+{
+    /*
+        NOTE: this tool must be run from the main project directory
+    */
+
+    class Program
+    {
+        static List<Build> s_builds;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            { 
+                Console.Write("\nSpecify input list file\n");
+                Console.Write("\nExample: autojoin input_file_list.aj output.html\n");
+
+                return;
+            }
+
+            if (args.Length == 1)
+            {
+                Console.Write("\nSpecify output file\n");
+                Console.Write("\nExample: autojoin input_file_list.aj output.html\n");
+                return;
+            }
+            
+
+            bool watch = 
+                   args.Length % 2 == 1
+                && args[args.Length-1] == "-watch";
+
+
+            if (watch)
+                Console.Write("\n");
+
+
+            s_builds = new List<Build>();
+
+            var i = 0;
+            while (args.Length >= i + 2)
+            { 
+                var build = new Build(args[i], args[i+1]);
+                if (watch) build.Watch();
+                s_builds.Add(build);
+                i += 2;
+            }
+
+
+            if (watch)
+            { 
+                Console.Write("\n");
+                Console.ReadKey();
+            }
+        }
+    }
+}
